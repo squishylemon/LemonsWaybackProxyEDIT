6d58d1423f3a033d86a46a6991169240a5841f45
squishylemon <63502775+squishylemon@users.noreply.github.com>
2023-02-19 23:16:53 +1100

    newfix

--- config_handler.py
+++ config_handler.py
@@ -1,44 +1,16 @@
 import json
 import datetime
+import socket
+import time
 
-# Listen port for the HTTP proxy.
 global LISTEN_PORT
-
-# Date to get pages from Wayback. YYYYMMDD, YYYYMM and YYYY formats are
-# accepted, the more specific the better.
 global DATE 
-
-# Allow the client to load pages and assets up to X days after DATE.
-# Set to None to disable this restriction.
 global DATE_TOLERANCE
-
-# Send Geocities requests to oocities.org if set to True.
 global GEOCITIES_FIX
-
-# Use the original Wayback Machine URL as a shortcut when loading images.
-# May result in faster page loads, but all images will point to
-# http://web.archive.org/... as a side effect. Set this value to 2 to enable an
-# experimental mode using authentication on top of the original URLs instead
-# (which is not supported by Internet Explorer and some other browsers).
 global QUICK_IMAGES
-
-# Use the Wayback Machine Availability API to find the closest available
-# snapshot to the desired date, instead of directly requesting that date. Helps
-# in situations where an image returns a server error on the desired date, but
-# is available at an earlier date. As a side effect, pages will take longer to
-# load due to the added API call. If enabled, this option will disable the
-# QUICK_IMAGES bypass mechanism built into the PAC file.
 global WAYBACK_API
-
-# Allow the Content-Type header to contain an encoding. Some old browsers
-# (Mosaic?) don't understand that and fail to load anything - set this to
-# False if you're using one of them.
 global CONTENT_TYPE_ENCODING
-
-# Disables logging if set to True.
 global SILENT
-
-# Enables the settings page on http://web.archive.org if set to True.
 global SETTINGS_PAGE
 
 try:
@@ -55,4 +27,23 @@ try:
 except EnvironmentError as e:
     print("Wops! Error opening config.json")
 
-DATE = datetime.datetime.now().strftime('%Y%m%d')
+def get_remote_date():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)
+            s.connect(('10.0.0.2', 123))
+            data = s.recv(1024)
+            remote_time = datetime.datetime.fromtimestamp(int.from_bytes(data[40:48], byteorder='big'))
+            return remote_time.strftime('%Y%m%d')
+    except Exception as e:
+        print(e)
+        return '19991231'
+
+DATE = get_remote_date()
+
+while True:
+    time.sleep(5)
+    new_date = get_remote_date()
+    if new_date != DATE:
+        DATE = new_date
+        print("Date updated to: ", DATE)
